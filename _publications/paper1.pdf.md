98a8cc29a571132c7b7981df8084142b2d87b8c8
qiao19881314 <qiao19881314@gmail.com>
2023-02-21 16:35:36 +0800

    更改第一篇文章的信息

---
+++ _publications/paper1.pdf.md
@@ -0,0 +1,12 @@
+---
+title: "Cattle body detection based on YOLOv5-ASFF for precision livestock farming  Journal 1"
+collection: publications
+permalink: /publication/2009-10-01-http://qiao1314.github.io/files/paper1.pdf
+excerpt: 'Your Name, You. (2009). &quot;Paper Title Number 1.&quot; <i>Journal 1</i>. 1(1).'
+date: 2009-10-01
+venue: 'This paper is about the number 1. The number 2 is left for future work.'
+citation: 'paper-title-number-1'
+---
+Your Name, You. (2009). &quot;Paper Title Number 1.&quot; <i>Journal 1</i>. 1(1).
+
+Recommended citation: paper-title-number-1
--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -1,9 +1,9 @@
 
 # coding: utf-8
 
-# # Publications markdown generator for academicpages
+# # Publications markdown generator for qiao19881314
 # 
-# Takes a TSV of publications with metadata and converts them for use with [academicpages.github.io](academicpages.github.io). This is an interactive Jupyter notebook, with the core python code in publications.py. Run either from the `markdown_generator` folder after replacing `publications.tsv` with one that fits your format.
+# Takes a TSV of publications with metadata and converts them for use with [qiao19881314.github.io](qiao19881314.github.io). This is an interactive Jupyter notebook, with the core python code in publications.py. Run either from the `markdown_generator` folder after replacing `publications.tsv` with one that fits your format.
 # 
 # TODO: Make this work with BibTex and other databases of citations, rather than Stuart's non-standard TSV format and citation style.
 # 
@@ -36,7 +36,6 @@ import pandas as pd
 import os
 os.chdir("markdown_generator")
 publications = pd.read_csv("publications.tsv", sep="\t", header=0)
-publications
 
 
 # ## Escape special characters
