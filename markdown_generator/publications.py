baa3359e64a07b00e75668417b21fbfd4f235147
qiao19881314 <qiao19881314@gmail.com>
2023-03-27 16:59:35 +0800

    更改github名字

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -1,9 +1,9 @@
 
 # coding: utf-8
 
-# # Publications markdown generator for qiao19881314
+# # Publications markdown generator for yongliang-qiao
 # 
-# Takes a TSV of publications with metadata and converts them for use with [qiao19881314.github.io](qiao19881314.github.io). This is an interactive Jupyter notebook, with the core python code in publications.py. Run either from the `markdown_generator` folder after replacing `publications.tsv` with one that fits your format.
+# Takes a TSV of publications with metadata and converts them for use with [yongliang-qiao.github.io](yongliang-qiao.github.io). This is an interactive Jupyter notebook, with the core python code in publications.py. Run either from the `markdown_generator` folder after replacing `publications.tsv` with one that fits your format.
 # 
 # TODO: Make this work with BibTex and other databases of citations, rather than Stuart's non-standard TSV format and citation style.
 # 
